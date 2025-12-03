bc750417c4b700aa4ae0be2213fd4a14fdfdc003
Thibault Soulabaille <t.soulabaille@fairvision.fr>
2025-12-03 14:47:41 +0100

    add day 3 part 2

--- advent-of-code/day_3.py
+++ advent-of-code/day_3.py
@@ -19,6 +19,29 @@ def part_1(input_: list[str]) -> int:
 @check_time
 def part_2(input_: list[str]) -> int:
     res = 0
+    len_jolt = 12
+    for bank in input_:
+        jolt = ""
+        bank_int = [int(bat) for bat in bank]
+
+        max_bat = bank_int[0]
+        max_bat_idx = 1
+
+        for i in range(len_jolt):
+            bat_list = bank_int[max_bat_idx : len(bank) + 1 - (len_jolt - i)]
+
+            curr_max_bat_idx = 0
+            for bat_idx, bat in enumerate(bat_list):
+                if bat > max_bat:
+                    max_bat = bat
+                    curr_max_bat_idx = bat_idx + 1
+
+            max_bat_idx += curr_max_bat_idx
+
+            jolt += str(max_bat)
+            max_bat = 0
+
+        res += int(jolt)
     return res
 
 
