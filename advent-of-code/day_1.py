aaa2d47043d5b74efc2830f2d56b7ae808101bc8
thibaultSoulabaille <thibault.soulabaille@gmail.com>
2025-12-03 19:39:06 +0100

    add day 1 part 1

---
+++ advent-of-code/day_1.py
@@ -0,0 +1,30 @@
+import utils.aoc_utils as aoc_utils
+from utils import check_time
+
+
+@check_time
+def part_1(input_: list[str]) -> int:
+    res = 0
+    pos = 50
+    for move in input_:
+        direction = move[0]
+        distance = int(move[1:])
+
+        if direction == "L":
+            distance *= -1
+
+        pos = (pos + distance) % 100
+        res += (pos == 0)
+    return res
+
+
+@check_time
+def part_2(input_: list[str]) -> int:
+    res = 0
+    return res
+
+
+if __name__ == "__main__":
+    input_ = aoc_utils.open_input(day=1, test=False)
+    print(f"Part 1: {part_1(input_)}")
+    print(f"Part 2: {part_2(input_)}")
