f2b31dfe67ac66997aa5cbd97ddbf128a49152ae
Thibault Soulabaille <t.soulabaille@fairvision.fr>
2025-12-04 16:34:16 +0100

    add day 4 part 2

--- advent-of-code/day_4.py
+++ advent-of-code/day_4.py
@@ -2,16 +2,26 @@ import utils.aoc_utils as aoc_utils
 from utils import check_time
 
 
+def preprocess_input(input_: list[str]) -> list[list[int]]:
+    input_preproc = [list(map(int, line)) for line in input_]
+
+    input_preproc.insert(0, [0] * len(input_[0]))
+    input_preproc.append([0] * len(input_[0]))
+
+    return input_preproc
+
+
+def count_grid(grid: list[list[int]]) -> int:
+    return sum([sum(row) for row in grid])
+
+
 @check_time
 def part_1(input_: list[str]) -> int:
     res = 0
 
-    input_int = [list(map(int, line)) for line in input_]
+    input_int = preprocess_input(input_)
 
     grid_w, grid_h = len(input_[0]), len(input_)
-
-    input_int.insert(0, [0] * grid_w)
-    input_int.append([0] * grid_w)
 
     for i, line in enumerate(input_int[1 : grid_h + 1]):
         for j, roll in enumerate(line):
@@ -43,6 +53,44 @@ def part_1(input_: list[str]) -> int:
 @check_time
 def part_2(input_: list[str]) -> int:
     res = 0
+
+    input_int = preprocess_input(input_)
+    grid_count = count_grid(input_int)
+
+    grid_w, grid_h = len(input_[0]), len(input_)
+
+    while True:
+        for i, line in enumerate(input_int[1 : grid_h + 1]):
+            for j, roll in enumerate(line):
+                if roll == 0:
+                    continue
+
+                total = 0
+                if j == 0:
+                    total += sum(input_int[i][:2]) + sum(input_int[i + 2][:2]) + line[j + 1]
+                elif j == grid_w - 1:
+                    total += (
+                        sum(input_int[i][grid_w - 2 :])
+                        + sum(input_int[i + 2][grid_w - 2 :])
+                        + line[j - 1]
+                    )
+                else:
+                    total += (
+                        sum(input_int[i][j - 1 : j + 2])
+                        + sum(input_int[i + 2][j - 1 : j + 2])
+                        + line[j - 1]
+                        + line[j + 1]
+                    )
+
+                if total < 4:
+                    res += 1
+                    input_int[i + 1][j] = 0
+
+        curr_count_grid = count_grid(input_int)
+        if curr_count_grid == grid_count:
+            break
+        grid_count = curr_count_grid
+
     return res
 
 
