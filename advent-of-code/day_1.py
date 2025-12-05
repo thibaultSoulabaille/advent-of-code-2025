1513a2e56d206e5c925f45992fa18dc986e00de4
thibaultSoulabaille <thibault.soulabaille@gmail.com>
2025-12-05 19:58:04 +0100

    add day 1 part 2

--- advent-of-code/day_1.py
+++ advent-of-code/day_1.py
@@ -21,6 +21,24 @@ def part_1(input_: list[str]) -> int:
 @check_time
 def part_2(input_: list[str]) -> int:
     res = 0
+    pos = 50
+    for move in input_:
+        direction = move[0]
+        distance = int(move[1:])
+
+        if direction == "L":
+            distance *= -1
+
+        q = 0
+        if pos == 0:
+            q, _ = divmod(abs(pos + distance), 100)
+        elif distance > 0:
+            q, _ = divmod(pos + distance, 100)
+        else:
+            q, _ = divmod(100 - pos + abs(distance), 100)
+        res += abs(q)
+
+        pos = (pos + distance) % 100
     return res
 
 
