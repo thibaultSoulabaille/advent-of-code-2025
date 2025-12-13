fcf204e2564213c2f14201c4bfd0176cda541580
thibaultSoulabaille <thibault.soulabaille@gmail.com>
2025-12-13 15:13:10 +0100

    add day 11 part 2 & update part 1

--- advent-of-code/day_11.py
+++ advent-of-code/day_11.py
@@ -1,31 +1,41 @@
 import utils.aoc_utils as aoc_utils
 from utils import check_time
+from functools import lru_cache
 
 
 @check_time
-def part_1(input_: list[str]) -> int:
-    devices_paths_dict = {
-        line.split(":")[0]: line.split(":")[1].split() for line in input_
-    }
-    res = 0
+def day_11(input_: list[str]) -> tuple[int, int]:
+    devices_paths = {line.split(":")[0]: line.split(":")[1].split() for line in input_}
 
-    paths = ["you"]
-    while len(paths) > 0:
-        device = paths.pop(0)
-        if device == "out":
-            res += 1
-        else:
-            paths.extend(devices_paths_dict[device])
-    return res
+    @lru_cache
+    def count_paths(start: str, end: str) -> int:
+        """use memoized recursive DFS to count paths from given start to finish device"""
+        if start == end:
+            return 1
+        return sum(count_paths(dst, end) for dst in devices_paths.get(start, []))
 
+    ######## PART 1
+    res_1 = count_paths("you", "out")
 
-@check_time
-def part_2(input_: list[str]) -> int:
-    res = 0
-    return res
+    ######## PART 2
+    svr_fft_path = count_paths("svr", "fft")
+    fft_dac_path = count_paths("fft", "dac")
+    dac_out_path = count_paths("dac", "out")
+
+    svr_dac_path = count_paths("svr", "dac")
+    dac_fft_path = count_paths("dac", "fft")
+    fft_out_path = count_paths("fft", "out")
+
+    res_2 = (
+        svr_fft_path * fft_dac_path * dac_out_path
+        + svr_dac_path * dac_fft_path * fft_out_path
+    )
+
+    return res_1, res_2
 
 
 if __name__ == "__main__":
     input_ = aoc_utils.open_input(day=11, test=False, as_list=True)
-    print(f"Part 1: {part_1(input_)}")
-    print(f"Part 2: {part_2(input_)}")
+    part_1, part_2 = day_11(input_)
+    print(f"Part 1: {part_1}")
+    print(f"Part 2: {part_2}")
